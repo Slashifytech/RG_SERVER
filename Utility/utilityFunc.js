7b6f775afbfab4f1d8cde48287c8cfe5170364d7
Minhaz Ashraf <minhazashraf590@gmail.com>
2025-01-25 23:37:44 +0530

    updated code

--- Utility/utilityFunc.js
+++ Utility/utilityFunc.js
@@ -25,7 +25,7 @@ const decryptText = (encryptedText) => {
    const formatIsoDate = (isoDate) => {
     const date = new Date(isoDate);
     const day = String(date.getDate()).padStart(2, '0'); 
-    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const month = String(date.getMonth() + 1).padStart(2, '0')
     const year = date.getFullYear();
     
     return `${day}/${month}/${year}`; 
